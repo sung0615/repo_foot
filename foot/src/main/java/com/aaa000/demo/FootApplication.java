b75b8141b1b44542e599a6991b1c0661deb7b55d
sung0615 <kk1hhh1fff1@naver.com>
2025-03-10 11:00:09 +0900

    frist

---
+++ foot/src/main/java/com/aaa000/demo/FootApplication.java
@@ -0,0 +1,13 @@
+package com.aaa000.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootApplication.class, args);
+	}
+
+}
---
+++ foot/src/test/java/com/aaa000/demo/FootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaa000.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
